652b1564d1fe26b6efe7f11a0360dff6ea80efba
David Alm <sensiblearts@gmail.com>
2025-11-30 13:29:47 -0500

    tweak

--- src/main.rs
+++ src/main.rs
@@ -108,8 +108,10 @@ impl Person {
     }
     
     fn transition(&mut self, event: Event) {
-        let a_fn = Event::dispatch(event);  // get the function              
-        let new_state = a_fn(self); // call the function
+        // let a_fn = Event::dispatch(event);  // get the function              
+        // let new_state = a_fn(self); // call the function
+        let new_state = Event::dispatch(event)(self);  // get and call the function              
+       
         self.previous_state = self.current_state.clone();
         self.current_state = new_state;
 
